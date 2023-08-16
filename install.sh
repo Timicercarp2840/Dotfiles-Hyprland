b3084d1134be7d650a61d5698a437e9b87a61892
Timicercarp2840 <136396501+Timicercarp2840@users.noreply.github.com>
2023-08-16 10:27:53 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo mv -f config.fish ~/.config/fish/
+sudo mv -f grub/themes/CyberRe/ /boot/grub/themes/
+sudo mv -f grub/grub /etc/default/
+sudo update-grub
+sudo mv -f hypr/ ~/.config/
+sudo mv -f sddm/themes/sugar-dark/ /usr/share/sddm/themes
+sudo mv -f sddm/sddm.conf /etc/sddm.conf.d/
